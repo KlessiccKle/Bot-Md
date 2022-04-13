122c2ef41230b6ca958a456b3128465d5501f21b
KlessiccKle <103492721+KlessiccKle@users.noreply.github.com>
2022-04-13 15:42:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ import { fileURLToPath } from 'url'
 global.owner = [
   ['0'],
   ['0'],
-  ['6282124128334', 'RedstoneCraft', '6281211006838', 'Klebitz' false]
+  ['6282124128334', 'RedstoneCraft', '6281211006838', 'Klebitz' true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['6281211006838'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
