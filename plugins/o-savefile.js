9cf15225c6e08b19a7911747ce81300030860418
KlessiccKle <103492721+KlessiccKle@users.noreply.github.com>
2022-04-15 08:13:13 +0700

    Update o-savefile.js

--- plugins/o-savefile.js
+++ plugins/o-savefile.js
@@ -1,5 +1,7 @@
+// ALYAAXZY RECODE
+
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/melcanz.js`
+    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/sticker.js`
     if (!m.quoted.text) throw `balas pesan nya!`
     let path = `${text}`
     await require('fs').writeFileSync(path, m.quoted.text)
@@ -9,6 +11,6 @@ handler.help = ['sf'].map(v => v + ' <teks>')
 handler.tags = ['owner']
 handler.command = /^sf$/i
 
-handler.rowner = true
+handler.owner = true
 
 module.exports = handler
